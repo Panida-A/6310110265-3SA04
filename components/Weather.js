b75549fe28d87ba5ae37c2642652b5a44c24393f
Panida Auitrakun <panidaauitrakun1234@gmail.com>
2022-08-26 21:36:25 +0700

    Components & Image bg

--- components/Weather.js
+++ components/Weather.js
@@ -22,11 +22,10 @@ export default function Weather(props) {
 }
 
 const styles = StyleSheet.create ({
+
     backdrop: {
-        //flexDirection: 'column',
-        //justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
-        height: '100%'
-    }
+        height: '100%',
+    }    
 })
