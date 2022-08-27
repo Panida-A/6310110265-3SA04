9585af1dd2b56b26f3a22e094569c728c2b407c1
Panida Auitrakun <panidaauitrakun1234@gmail.com>
2022-08-27 22:47:19 +0700

    Part 6.3

--- screen/WeatherScreen.js
+++ screen/WeatherScreen.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 import Weather from "../components/Weather";
 
 export default function WeatherScreen ({route}) {
     return (
         <View>
             <Weather zipCode = {route.params.zipCode}/>
+            <StatusBar style = "auto"/>
         </View>
+        
     )
 }
+
+const styles = StyleSheet.create({
+    wtIcon: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,15 +1,24 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 import { TouchableHighlight } from "react-native-gesture-handler";
 
 const availableZipItems = [
-
     { place: 'Hatyai',  code: '90110' },
     { place: 'Trang',   code: '92000' },
     { place: 'Chiangmai',   code: '50000' },
     { place: 'Khonkaen',    code: '40000' },
     { place: 'Chonburi',    code: '20000' },
+    { place: 'Krabi',   code: '81000' },
+    { place: 'Chanthaburi', code: '22000' },
+    { place: 'Samut Prakan', code: '10270' },
+    { place: 'Sukhothai', code: '64000' },
+    { place: 'Trat', code: '23000' },
+    { place: 'Rayong', code: '21000' },
+    { place: 'Nakhon Ratchasima', code: '30000' },
+    { place: 'Buriram', code: '31000' },
+    { place: 'Phuket', code: '83000' },
+    { place: 'Surat Thani', code: '84000' },
 
 ]
 
@@ -26,15 +35,14 @@ const ZipItem = ({place, code, navigation}) => (
 
 const _keyExtractor = item => item.code
 
-export default function ZipCodeScreen() {   
-    
+export default function ZipCodeScreen() {
     const navigation = useNavigation()
 
-    return (
+    return(
         <FlatList
-            data = {availableZipItems}
+            data={ availableZipItems }
             keyExtractor = {item => item.code}
-            renderItem = {({item}) => <ZipItem {...item} navigation = {navigation}/>}
+            renderItem = {({item}) => <ZipItem {...item} navigation = {navigation} /> }
         />
     )
 }
